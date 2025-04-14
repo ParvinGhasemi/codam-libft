4dd31c87ee2976ab0c5678097dcf181b61ec1989
parvinghasemi <mghasemi.parvin@gmail.com>
2025-04-14 20:46:24 +0200

    adding libft.h

---
+++ libft.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   libft.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/04/13 12:08:32 by parvinm.gha   #+#    #+#                 */
+/*   Updated: 2025/04/14 20:45:34 by parvinm.gha   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+
+int		ft_isdigit(int c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putchar(char c);
+void	ft_putnbr_fd(int nb, int fd);
+void	ft_putnbr(int nb);
+void	ft_putstr_fd(char const *str, int fd);
+void	ft_putstr(char const *str);
+int		ft_isupper(int c);
+int		ft_islower(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_isalpha(int c);
+char	*ft_strstr(char *str, char *to_find);
+
+// isalnum, iswupper
+// tolower_l, 
+
+#endif
