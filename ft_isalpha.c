ad495cb351369078b5214b5cb254fa3108e89367
parvinghasemi <mghasemi.parvin@gmail.com>
2025-04-14 20:36:22 +0200

    norminette check the ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,13 +6,13 @@
 /*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/13 14:39:04 by parvinm.gha   #+#    #+#                 */
-/*   Updated: 2025/04/13 14:39:59 by parvinm.gha   ########   odam.nl         */
+/*   Updated: 2025/04/14 20:34:35 by parvinm.gha   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_isalpha(int c)
+int	ft_isalpha(int c)
 {
 	return (ft_islower(c) || ft_isupper(c));
 }
