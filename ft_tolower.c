1f625cd6f73eba1c699d6cef416a34df73867ef3
parvinghasemi <mghasemi.parvin@gmail.com>
2025-04-14 20:40:29 +0200

    adding ft_tolower.c

---
+++ ft_tolower.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_tolower.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/04/13 14:24:23 by parvinm.gha   #+#    #+#                 */
+/*   Updated: 2025/04/13 14:32:27 by parvinm.gha   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_tolower(int c)
+{
+	if (ft_isupper)
+		return (c + 32);
+	return (c);
+}
