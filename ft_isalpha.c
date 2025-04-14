eb5b329c90a0c7253ad766058ba45efee5e0a24a
parvinghasemi <mghasemi.parvin@gmail.com>
2025-04-14 20:27:12 +0200

    adding ft_isalpha.c

---
+++ ft_isalpha.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_isalpha.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/04/13 14:39:04 by parvinm.gha   #+#    #+#                 */
+/*   Updated: 2025/04/13 14:39:59 by parvinm.gha   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_isalpha(int c)
+{
+	return (ft_islower(c) || ft_isupper(c));
+}
